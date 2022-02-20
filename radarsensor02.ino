0e69a85f1224fed663d317ec751df5d1efa8e29c
Robert Simai <rsimai@suse.com>
2022-02-20 15:56:20 +0100

    initial

---
+++ radarsensor02.ino
@@ -0,0 +1,26 @@
+#include <DigiCDC.h>
+
+int in;
+
+void setup() {                
+  // initialize 
+  SerialUSB.begin();
+  pinMode(0, OUTPUT); //module power
+  pinMode(1, OUTPUT); //internal LED
+  pinMode(2, INPUT);  //motion detected
+  digitalWrite(0, HIGH); //switch module on
+}
+
+void loop() {
+  in = digitalRead(2);
+  if (in == HIGH) {
+    digitalWrite(1, HIGH);
+    if (SerialUSB.available()) {
+      SerialUSB.println('a');
+    }
+  }
+  else {
+    digitalWrite(1, LOW);
+  }
+   SerialUSB.delay(100);               // keep usb alive // can alos use SerialUSB.refresh();
+}
